e45b040783cee4ece345e018a2a23e4505fa204d
Samuel Grayson <sam@samgrayson.me>
2022-11-08 14:04:32 -0600

    Removed setarch from script

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 directory=$(dirname $0)
-env FAKETIME='2022-01-01 00:00:00' LD_PRELOAD=/usr/lib/x86_64-linux-gnu/faketime/libfaketime.so.1:$directory/deterministic_random_preload.so setarch $(arch) --addr-no-randomize $@
+env FAKETIME='2022-01-01 00:00:00' LD_PRELOAD=/usr/lib/x86_64-linux-gnu/faketime/libfaketime.so.1:$directory/deterministic_random_preload.so $@
