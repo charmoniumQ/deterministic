ffff77e3081107039f9130772f7d04bbe5554f3e
Samuel Grayson <sam@samgrayson.me>
2022-11-08 13:53:26 -0600

    Change script to not assume PWD

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-env FAKETIME='2022-01-01 00:00:00' LD_PRELOAD=/usr/lib/x86_64-linux-gnu/faketime/libfaketime.so.1:$PWD/deterministic_random_preload.so setarch $(arch) --addr-no-randomize $@
+directory=$(dirname $0)
+env FAKETIME='2022-01-01 00:00:00' LD_PRELOAD=/usr/lib/x86_64-linux-gnu/faketime/libfaketime.so.1:$directory/deterministic_random_preload.so setarch $(arch) --addr-no-randomize $@
